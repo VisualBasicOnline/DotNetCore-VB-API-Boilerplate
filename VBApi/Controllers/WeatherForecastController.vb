23ac3b0a662398182f3bf34a5141d688eac9e895
Craig Peacock <craig.peacock@outlook.com>
2023-10-13 13:42:40 +0100

    Initial Commit

---
+++ VBApi/Controllers/WeatherForecastController.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.AspNetCore.Mvc
+Imports Microsoft.Extensions.Logging 
+
+<ApiController>
+<Route("[controller]")>
+Public Class WeatherForecastController
+    Inherits ControllerBase
+
+    Private Shared ReadOnly Summaries As String() =
+                                {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering",
+                                 "Scorching"}
+
+    Private ReadOnly _logger As ILogger(Of WeatherForecastController)
+
+    Public Sub New(ByVal logger As ILogger(Of WeatherForecastController))
+        _logger = logger
+    End Sub
+
+    <HttpGet("GetWeatherForecast")>
+    Public Function GetForecast() As IEnumerable(Of WeatherForecastViewModel)
+
+        Return Enumerable.Range(1, 5).
+            Select(
+                Function(index) New WeatherForecastViewModel With 
+                      {
+                      .ForecastDate  = DateTime.Now.AddDays(index),
+                      .TemperatureC = Random.Shared.Next(- 20, 55),
+                      .Summary = Summaries(Random.Shared.Next(Summaries.Length))
+                      }
+                ).ToArray()
+    End Function
+End Class
---
+++ VBApi/Models/WeatherForecastViewModel.vb
@@ -0,0 +1,12 @@
+﻿Public Class WeatherForecastViewModel
+    Public Property ForecastDate As DateTime
+    Public Property TemperatureC As Integer
+
+    Public ReadOnly Property TemperatureF As Integer
+        Get
+            Return 32 + CInt((TemperatureC / 0.5556))
+        End Get
+    End Property
+
+    Public Property Summary As String
+End Class
